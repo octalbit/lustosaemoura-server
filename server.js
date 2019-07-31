f3255d4ac5283e4fb60d068e31dc41cdd3c6fd13
rafaelangical <rafaelangical2@gmail.com>
2019-07-31 19:03:43 -0300

    Add server

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+const Post = require('./api/models/postModel'); //created model loading here
+const bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect(`${process.env.DB_URL}`); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use(function(req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// });
+
+const routes = require('./api/routes/postRoutes'); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+console.log(`server listening on ${port}`);
