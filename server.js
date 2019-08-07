83b876ec32ad89b9dbcd2dd175894916ba37afe1
rafaelangical <rafaelangical2@gmail.com>
2019-08-07 20:56:28 -0300

    Enable cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
