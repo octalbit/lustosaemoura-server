b5027c7f08589ba9969a5be533ed79fe1628d454
rafaelangical <rafaelangical2@gmail.com>
2019-08-07 20:47:44 -0300

    Add headers

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const bodyParser = require('body-parser');
 mongoose.Promise = global.Promise;
 mongoose.connect(`${process.env.DB_URL}`); 
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
